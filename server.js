90c57d694c1acff0fdfa67f453df14da41b6e898
Hung <nthung26112007@gmail.com>
2026-07-10 11:32:10 +0700

    Add manual CORS headers for Render proxy

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Vary', 'Origin');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept, X-Requested-With');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
     }
